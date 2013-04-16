7337f6edfd0c35d5af9e816223d055fda1c90fba
Manyuuz <manourm@debian.localdomain>
2013-04-16 14:24:33 +0200

    Ajout de l'abstraction des acces concurrentiel

---
+++ src/CompetitiveAccessAbstraction/UnixCondVar.cpp
@@ -0,0 +1,25 @@
+#include "UnixCondVar.hpp"
+
+UnixCondVar::UnixCondVar()
+{
+}
+
+UnixCondVar::~UnixCondVar()
+{
+}
+
+int	UnixCondVar::signal()
+{
+}
+
+int	UnixCondVar::broadcast()
+{
+}
+
+int	UnixCondVar::wait()
+{
+}
+
+int	UnixCondVar::timedwait()
+{
+}
---
+++ src/CompetitiveAccessAbstraction/UnixThread.cpp
@@ -0,0 +1,1 @@
+#include "UnixThread.hpp"

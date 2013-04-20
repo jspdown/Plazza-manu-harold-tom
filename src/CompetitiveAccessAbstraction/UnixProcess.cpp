5aa2cb34acca0a3f7b19b934070b182488c33476
Manyuuz <manourm@debian.localdomain>
2013-04-20 15:11:55 +0200

    rgj

--- src/CompetitiveAccessAbstraction/UnixProcess.cpp
+++ src/CompetitiveAccessAbstraction/UnixProcess.cpp
@@ -1,4 +1,5 @@
 #include "UnixProcess.hh"
+#include <algorithm>
 
 UnixProcess::UnixProcess()
 {
@@ -14,7 +15,7 @@ UnixProcess::~UnixProcess()
 {
 }
 
-IProcess *	UnixProcess::fork()
+IProcess *		UnixProcess::fork()
 {
   pid_t	pid;
 
@@ -24,11 +25,31 @@ IProcess *	UnixProcess::fork()
       std::cerr << "Error : 'fork' failed." <<std::endl;
       return 0;
     }
-  else
+  else if (pid == 0)
     return new UnixProcess(pid);
 }
 
-void		UnixProcess::exit(int return_value)
+void			UnixProcess::exit(int return_value)
 {
   exit(return_value)
+}
+
+void			UnixProcess::setPipe(std::pair<NamedPipe, NamedPipe> & pipes)
+{
+  this->pipes = pipes;
+}
+
+void			UnixProcess::put(const std::string & msg)
+{
+  std::vector<std::string>	trame;
+
+  trame = CmdLineParser::CmdLineToTrame(msg);
+  for_each(trame.begin(), trame.end(), (this->pipes.first())->put);
+}
+
+const std::string &    	UnixProcess::get()
+{
+  std::string	msg;
+
+  msg = (this->pipes.second())->get;
 }
